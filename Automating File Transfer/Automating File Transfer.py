ec8498bc8d975f057d4eaf867b18ca3d76c1632d
Matheus Pedreira dos Santos França <38478879+franca072@users.noreply.github.com>
2023-01-20 20:09:40 -0300

    File Transfer Code

--- Automating File Transfer/Automating File Transfer.py
+++ Automating File Transfer/Automating File Transfer.py
@@ -16,34 +16,63 @@ Here are the steps you can take to automate this process:
 
     You can also set up a log file to keep track of the files that have been transferred and any errors that may have occurred during the transfer process. '''
 
-
 from ftplib import FTP
+from datetime import datetime
+import os, shutil, schedule, time
 
-host = "ftp.dlptest.com"
-user = "dlpuser"
-password = "rNrKYTX9g7z3RgJRmxWuGHbeu"  #should be taken from an external file, preferably encrypted
+HOST = "ftp.dlptest.com"
+TEMP_FOLDER = f"{os.getcwd()}/.temp/"
+USER, PASSWORD = "dlpuser","rNrKYTX9g7z3RgJRmxWuGHbeu"    # Provided by the FTP server
+destination = input("Insert the network location to store downloaded data:\n")    # Gets input from user to save downloaded files on desired location.
 
-src_dir = ""
-dest_dir = "somedir/someotherdir"
+def download(ftp,log,files=[],destination=""):
+    for file in files:
+        try:
+            if file not in os.listdir(destination):    # Only downloads files that have not been previously downloaded.
+                print(f"Downloading {file}...")
+                ftp.retrbinary(f"RETR {file}",open(f"{TEMP_FOLDER}/{file}","wb").write)    # Downloads files from FTP server
+                log.write(f"Downloaded {file}.\n")
+        except Exception as e:
+            msg = f"Error while trying to download {file}"
+            log.write(f"{msg} -> {e}\n")
+            print(msg)
 
-ftp = FTP(host, user=user, passwd=password)
-if src_dir != "":
-    ftp.cwd(src_dir)
+def move(log,files=[],destination=""):
+    for file in files:
+        try:
+            print(f"Moving {file}...")
+            shutil.move(f"{TEMP_FOLDER}/{file}",f"{destination}/{file}")    # Moves downloaded files from local storage to internal network folder
+            log.write(f"Moved {file}.\n")
+        except Exception as e:
+            msg = f"Error moving {file}."
+            log.write(f"{msg} -> {e}")
+            print(msg)
 
-if dest_dir != "":
-    #check existence of folder(s) create if necessary and move to it
-    pass
-#get the files list
-#files = ftp.mlsd()
-files = []
-ftp.retrlines('NLST', files.append)
-print(files)
+def main():
+    ftp = FTP(HOST,USER,PASSWORD)    # Connects to the FTP server
+    
+    log_location = f"{destination}/log.txt"
 
-for file in files:
-    #check existence
-    #log
-    with open(file, 'wb') as fp:
-        ftp.retrbinary('RETR {}'.format(file), fp.write)
-    #break
+    if not os.path.exists(TEMP_FOLDER):    # Creates a temporary directory to store downloaded files while not finished
+        os.makedirs(TEMP_FOLDER)
 
-ftp.quit()
+    if not os.path.exists(log_location):    # Determines if the log will be created or simply updated
+        log = open(log_location,"w")
+    else:
+        log = open(log_location,"a")
+
+    log.write(f"--> Start time: {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}\n")    # Tracks every download instance
+
+    files = ftp.nlst()    # Creates a list of files available to download in the FTP server
+    download(ftp,log,files,destination)
+    ftp.quit()
+
+    move(log,os.listdir(TEMP_FOLDER),destination)
+    log.close()
+    os.removedirs(TEMP_FOLDER)    # Deletes temporary folder
+
+schedule.every().day.at("20:00").do(main)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
