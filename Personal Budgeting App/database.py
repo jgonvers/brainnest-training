c056ed4de4b472963fa619e04fec502f77fd20eb
Andrey Volkov <andrey.dreamwall@gmail.com>
2023-01-21 18:15:31 +0300

    create models, fill transaction types

---
+++ Personal Budgeting App/database.py
@@ -0,0 +1,91 @@
+from sqlalchemy import (
+    CheckConstraint,
+    Column,
+    Date,
+    Float,
+    ForeignKey,
+    Integer,
+    String,
+    Text,
+    UniqueConstraint,
+    create_engine,
+)
+from sqlalchemy.orm import Session
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.exc import IntegrityError
+
+from settings import db_settings
+
+engine = create_engine(db_settings.db_url, echo=True)
+Base = declarative_base()
+session = Session(engine)
+
+
+class Transaction(Base):
+    __abstract__ = True
+
+    id = Column("id", Integer, primary_key=True)
+    amount = Column("amount", Float, nullable=False)
+    description = Column("description", Text, nullable=True)
+    date = Column("date", Date, nullable=False)
+
+    __table_args__ = (
+        CheckConstraint(amount > 0, name="check_amount_positive"),
+    )
+
+    def __init__(self, name):
+        self.name = name
+
+
+class TransactionType(Base):
+    __tablename__ = "transaction_types"
+
+    id = Column("id", Integer, primary_key=True)
+    name = Column("name", String(50), nullable=False)
+
+    __table_args__ = (UniqueConstraint(name, name="unique_name"),)
+
+
+class Income(Transaction):
+    __tablename__ = "incomes"
+
+    type = Column(
+        "type", Integer, ForeignKey("transaction_types.id"), default=1, nullable=False
+    )
+
+    def __init__(self, name):
+        super().__init__(name)
+
+
+class Expense(Transaction):
+    __tablename__ = "expenses"
+
+    type = Column(
+        "type", Integer, ForeignKey("transaction_types.id"), default=2, nullable=False
+    )
+
+    def __init__(self, name):
+        super().__init__(name)
+
+
+class Budget(Base):
+    __tablename__ = "budgets"
+
+    id = Column(Integer, primary_key=True)
+    amount = Column(Float, nullable=False)
+    start_date = Column(Date, nullable=False)
+    end_date = Column(Date, nullable=False)
+
+    __table_args__ = (
+        CheckConstraint(amount > 0, name="check_amount_positive"),
+    )
+
+
+Base.metadata.create_all(engine)
+
+types = [TransactionType(name="Income"), TransactionType(name="Expense")]
+try:
+    insert_types = session.bulk_save_objects(types)
+    session.commit()
+except IntegrityError:
+    session.rollback()
