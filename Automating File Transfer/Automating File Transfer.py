e4d992819231edbb009737dede545c85ddcc7885
Matheus França <mpsfranca@outlook.com.br>
2023-01-21 18:46:31 -0300

    Made some changes to follow PEP8

--- Automating File Transfer/Automating File Transfer.py
+++ Automating File Transfer/Automating File Transfer.py
@@ -1,8 +1,8 @@
 ''' You work at a company that receives daily data files from external partners. These files need to be processed and analyzed, but first, they need to be transferred to the company's internal network.
 
-The goal of this project is to automate the process of transferring the files from an external FTP server to the company's internal network.
+    The goal of this project is to automate the process of transferring the files from an external FTP server to the company's internal network.
 
-Here are the steps you can take to automate this process:
+    Here are the steps you can take to automate this process:
 
     Use the ftplib library to connect to the external FTP server and list the files in the directory.
 
@@ -17,37 +17,47 @@ Here are the steps you can take to automate this process:
     You can also set up a log file to keep track of the files that have been transferred and any errors that may have occurred during the transfer process. '''
 
 from ftplib import FTP
-import os, shutil, schedule, time, json, logging
+import json
+import logging
+import os
+import schedule
+import shutil
+import time
 
-# Global constants
 DAILY_TIME = "20:00"
-SRC_FOLDER = f"{os.getcwd()}/src/"
-TEMP_FOLDER = f"{os.getcwd()}/.temp/"
+SRC_FOLDER = os.path.join(os.getcwd(), "src")
+TEMP_FOLDER = os.path.join(os.getcwd(), ".temp")
+FORMAT = "%(asctime)s : %(levelname)s - %(message)s"
 
-# Logger configuration
-logging.basicConfig(level=logging.DEBUG,format="%(asctime)s : %(levelname)s - %(message)s")
+logging.basicConfig(level=logging.DEBUG, format=FORMAT)
 
-file_handler = logging.FileHandler(f"{SRC_FOLDER}/File Transfer.log")
-file_handler.setFormatter(logging.Formatter("%(asctime)s : %(levelname)s - %(message)s"))
+file_handler = logging.FileHandler(os.path.join(SRC_FOLDER,
+                                                "File Transfer.log"))
+file_handler.setFormatter(logging.Formatter(FORMAT))
 
 logger = logging.getLogger("AFT")
 logger.addHandler(file_handler)
 
-def download(ftp,files=[],destination=None):
+
+def download(ftp, files=[], downloaded_files=[]):
     for file in files:
         try:
-            if file not in os.listdir(destination) and file != "frep" and file != "input":    # Only downloads files that have not been previously downloaded. The frep and input files have been excluded since they give error due to lack of permission
+            if file not in downloaded_files and file not in ("frep", "input"):
                 logger.info(f"Downloading {file}...")
-                ftp.retrbinary(f"RETR {file}",open(f"{TEMP_FOLDER}/{file}","wb").write)    # Downloads files from FTP server
+                file_path = os.path.join(TEMP_FOLDER, file)
+                ftp.retrbinary(f"RETR {file}", open(file_path, "wb").write)
                 logger.info(f"Downloaded {file}.")
+
         except Exception as e:
             logger.exception(f"Error while trying to download {file}: {e}")
 
-def move(files=[],destination=None):
+
+def move(destination, files=[]):
     for file in files:
         try:
             logger.info(f"Moving {file}...")
-            shutil.move(f"{TEMP_FOLDER}/{file}",f"{destination}/{file}")    # Moves downloaded files from local storage to internal network folder
+            shutil.move(os.path.join(TEMP_FOLDER, file),
+                        os.path.join(destination, file))
             logger.info(f"Moved {file}.")
         except Exception as e:
             logger.exception(f"Error moving {file}: {e}")
@@ -55,14 +65,14 @@ def move(files=[],destination=None):
 
 def main():
     try:
-        file = open(f"{SRC_FOLDER}/settings.json")
+        file = open(os.path.join(SRC_FOLDER, "settings.json"))
         data = json.load(file)
 
         host = data["Host"]
         user = data["User"]
         password = data["Password"]
         destination = data["DownloadFolder"]
-        
+
         logger.info("Found settings file.")
     except Exception as e:
         logger.exception(f"Could not find settings file: {e}")
@@ -72,22 +82,22 @@ def main():
         os.makedirs(destination)
 
     try:
-        ftp = FTP(host,user,password)    # Connects to the FTP server
+        ftp = FTP(host, user, password)
         logger.info("Successfully connected to the FTP server.")
     except Exception as e:
         logger.exception(f"Failed to login: {e}")
         return
 
-    if not os.path.exists(TEMP_FOLDER):    # Creates a temporary directory to store downloaded files while not finished
+    if not os.path.exists(TEMP_FOLDER):
         os.makedirs(TEMP_FOLDER)
 
     try:
-        files = ftp.nlst()    # Creates a list of files available to download in the FTP server
+        files = ftp.nlst()
     except Exception as e:
         logger.exception(f"Failed to retrieve list of files: {e}")
         return
 
-    download(ftp,files,destination)
+    download(ftp, files, os.listdir(destination))
 
     try:
         ftp.quit()
@@ -96,13 +106,14 @@ def main():
         logger.exception("Failed to exit FTP server: {e}")
         return
 
-    move(os.listdir(TEMP_FOLDER),destination)
+    move(destination, os.listdir(TEMP_FOLDER))
 
     try:
-        os.removedirs(TEMP_FOLDER)    # Deletes temporary folder
+        os.removedirs(TEMP_FOLDER)
         logger.info("Successfully removed temporary folder.")
     except Exception as e:
         logger.exception("Error deleting temporary folder: {e}")
+
 
 schedule.every().day.at(DAILY_TIME).do(main)
 
