4ef818c7b3873a837d89449190256e89ffb0e6ad
Matheus França <mpsfranca@outlook.com.br>
2023-01-22 10:30:17 -0300

    Adding some comments

--- Automating Email Sending/Automating Email Sending.py
+++ Automating Email Sending/Automating Email Sending.py
@@ -23,12 +23,14 @@ from smtplib import SMTP
 from datetime import datetime
 from email.message import EmailMessage
 
+# Global Constants
 CWD = os.getcwd()
+DAILY_TIME = "10:57"
 SRC_FOLDER = os.path.join(CWD, "src")
 ATTACHMENT_FOLDER = os.path.join(CWD, "Reports")
 FORMAT = "%(asctime)s : %(levelname)s - %(message)s"
-DAILY_TIME = "10:57"
 
+# Logger configuration
 logging.basicConfig(level=logging.DEBUG, format=FORMAT)
 
 file_handler = logging.FileHandler(os.path.join(SRC_FOLDER,
@@ -40,8 +42,20 @@ logger.addHandler(file_handler)
 
 
 def create_email(subject, sender, receiver):
-    time_stamp = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+    '''
+    Returns a string representing the email created.
 
+            Parameters:
+                    subject (str): The subject of the email
+                    sender (str): The email address of the sender
+                    receiver (str): The email address of the receiver
+
+            Returns:
+                    e.as_string() (str): String representation of the email.
+    '''
+    time_stamp = datetime.now().strftime('%d/%m/%Y %H:%M:%S')    # Keeps track of the time the email was sent.
+
+    # Email creation
     e = EmailMessage()
     e["To"] = receiver
     e["From"] = "Brainnest - Automatic Email Sending"
@@ -54,7 +68,8 @@ def create_email(subject, sender, receiver):
     This email will be sent every 24 hours.
     """)
 
-    for file in os.listdir(ATTACHMENT_FOLDER):    # Attaches all files
+    # Attaching files
+    for file in os.listdir(ATTACHMENT_FOLDER):
         try:
             with open(os.path.join(ATTACHMENT_FOLDER, file), "rb") as f:
                 e.add_attachment(f.read(), maintype='application',
@@ -67,11 +82,23 @@ def create_email(subject, sender, receiver):
 
 
 def send(smtp, sender, receiver, email_obj):
+    '''
+    Sends the email to a given receiver.
+
+            Parameters:
+                    smtp : The SMTP server being used.
+                    sender (str): The email address of the sender.
+                    receiver (str): The email address of the receiver.
+                    email_obj (str): The email to be sent.
+
+            Returns:
+                    None
+    '''
     try:
         smtp.sendmail(sender, receiver, email_obj)
         logger.info(f"Email from {sender} has been sent to {receiver}")
     except Exception as ex:
-        logger.exception(f"Email from {sender} was"
+        logger.exception(f"Email from {sender} was "
                          f"not sent to {receiver}: {ex}.")
 
 
@@ -84,6 +111,7 @@ def main():
             logger.exception("Error starting TLS: {ex}.")
 
         try:
+            # Getting information about sender and receivers
             with open(os.path.join(SRC_FOLDER, "settings.json"), "r") as rf:
                 data = json.load(rf)
                 logger.info("Successfully loaded settings.")
@@ -92,6 +120,7 @@ def main():
 
         sender_email = data["SenderEmail"]
         sender_password = data["SenderPassword"]
+
         receiver_list = data["ReceiverList"]
 
         logger.debug(f"The sender email is: {sender_email}")
@@ -107,6 +136,7 @@ def main():
         for receiver in receiver_list:
             email_obj = create_email(subject, sender_email, receiver)
             send(smtp, sender_email, receiver, email_obj)
+
 
 schedule.every().day.at(DAILY_TIME).do(main)
 
