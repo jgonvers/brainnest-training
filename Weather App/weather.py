d8423d15656cf75ba0d44ccd849d6010c36664e0
Joachim Gonvers <joachim.gonvers@gmail.com>
2023-01-26 15:38:08 +0100

    add the the geocoding and added a get weather function for interacting

--- Weather App/weather.py
+++ Weather App/weather.py
@@ -11,8 +11,30 @@ from datetime import datetime
 
 class Weather():
     weather_API_url = "https://api.open-meteo.com/v1/forecast"
+    geocode_API_url = "https://geocode.xyz/"
+    geocode_API_key = "626551595926767797820x38247" #TODO put in a .env
     
-    def get_weather(self, coordinate, start_date, end_date):
+    
+    def get_weather(self, location, start_date, end_date):
+        location = self._get_location(location)
+        weather = self._get_weather_data(location["coordinate"], start_date, end_date)
+        return({"location":location["location"], "data":weather})
+    
+    def _get_location(self, location):
+        payload = {
+            "json":1,
+            "locate":location,
+            "auth":self.geocode_API_key
+            }
+        r = requests.get(self.geocode_API_url,params=payload)
+        return(self._parse_location(r.json()))
+    
+    def _parse_location(self, data):
+        coordinate = (data["latt"], data["longt"])
+        location = f'{data["standard"]["city"]}, {data["standard"]["countryname"]}'
+        return {"coordinate":coordinate, "location":location}
+        
+    def _get_weather_data(self, coordinate, start_date, end_date):
         weather_payload = { "timezone":"auto",
                             "daily": "weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,windspeed_10m_max,winddirection_10m_dominant".split(",") }
         weather_payload["latitude"] = coordinate[0]
@@ -22,33 +44,33 @@ class Weather():
         
         r = requests.get(self.weather_API_url, params=weather_payload)
         if r.status_code == 200:
-            return(self.json_convert(r.json()["daily"]))
+            return(self._json_convert(r.json()["daily"]))
         else: #error handling
             if r.status_code == 400:
                 return(r.json()["reason"])
             else:
                 return(f"HTML Error {r.status_code}")
 
-    def json_convert(self, res):
+    def _json_convert(self, res):
         converted = [dict() for _ in range(len(res["time"]))]
         for key in res:
             match key:
                 case "time"|"sunrise"|"sunset":
-                    self.distribute(list(map(datetime.fromisoformat, res[key])), key, converted)
+                    self._distribute(list(map(datetime.fromisoformat, res[key])), key, converted)
                 case "weathercode":
-                    self.distribute(list(map(self.convert_wmo, res[key])), key, converted)
+                    self._distribute(list(map(self._convert_wmo, res[key])), key, converted)
                 case "winddirection_10m_dominant":
-                    self.distribute(list(map(self.convert_wind_direction, res[key])), key, converted)
+                    self._distribute(list(map(self._convert_wind_direction, res[key])), key, converted)
                 case _:
-                    self.distribute(res[key], key, converted)
+                    self._distribute(res[key], key, converted)
         return(converted)
                     
             
-    def distribute(self, iter, key ,target_list):
+    def _distribute(self, iter, key ,target_list):
         for x in range(len(target_list)):
             target_list[x][key] = iter[x]
     
-    def convert_wind_direction(self,angle):
+    def _convert_wind_direction(self,angle):
         inc = 360/16
         if angle <= 1*inc and angle > 15*inc:
             return("North")
@@ -67,7 +89,7 @@ class Weather():
         elif angle <= 15*inc:
             return ("North-West")
     
-    def convert_wmo(self, code):
+    def _convert_wmo(self, code):
         match code:
             case 0|1:
                 return("clear")
@@ -88,5 +110,7 @@ class Weather():
         
 if __name__ == "__main__":
     w = Weather()
-    r = w.get_weather((46.5,6.45), "2023-01-26", "2023-01-30")
-    print(r)
+    r = w.get_weather("zurich, ch", "2023-01-26", "2023-01-30")
+    for key in r:
+        print(key)
+        print(r[key])
