8dfb455fc9cbeeb8c85f661236cee598f441e8ce
Andrey Volkov <andrey.dreamwall@gmail.com>
2023-01-29 19:58:15 +0300

    create logger

--- Weather App/main.py
+++ Weather App/main.py
@@ -1,4 +1,15 @@
+import logging
+
 from GUI_Weather import WeatherApp
+
+log_level = logging.INFO
+logger = logging.getLogger("main")
+logging.basicConfig(
+    filename="weather.log",
+    encoding="utf-8",
+    level=log_level,
+    format="%(asctime)s %(levelname)s: %(name)s: %(message)s",
+)
 
 
 def main():
@@ -6,8 +17,8 @@ def main():
         app = WeatherApp()
         app.start()
     except Exception as e:
-        # logger.error(e)
-        print("Error...")
+        logger.error(e)
+        pass
 
 
 if __name__ == "__main__":
