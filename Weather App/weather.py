d7d9bac18d761c81c1b67711e70b98a692d01ea9
Andrey Volkov <andrey.dreamwall@gmail.com>
2023-01-29 16:06:33 +0300

    fix throttling error

--- Weather App/weather.py
+++ Weather App/weather.py
@@ -8,6 +8,7 @@ except ImportError:
     import requests
 
 import os
+import time
 from dotenv import load_dotenv
 from weather_utils import WeatherUtils as wu
 
@@ -21,7 +22,7 @@ class Weather:
 
     def get_weather(self, location, start_date, end_date):
         location = self._get_location(location)
-        if location is not False:
+        if location:
             weather = self._get_weather_data(
                 location["coordinate"], start_date, end_date
             )
@@ -31,43 +32,60 @@ class Weather:
 
     def _get_location(self, location):
         payload = {"json": 1, "locate": location, "auth": self.geocode_API_key}
-        r = requests.get(self.geocode_API_url, params=payload)
-        if "matches" in r.json() and r.json()["matches"] == None:
+        response = requests.get(
+            self.geocode_API_url, params=payload, timeout=3
+        )
+        response_json = response.json()
+        # handle throttling
+        while (
+            response_json.get("error")
+            and response_json["error"]["code"] == "006"
+        ):
+            print("Throttling timeout. Retrying...")
+            time.sleep(2)
+            response_json = requests.get(
+                self.geocode_API_url, params=payload, timeout=3
+            ).json()
+        if "matches" in response_json and response_json["matches"] is None:
             return False
-        return wu.parse_location(r.json())
+        return wu.parse_location(response_json)
 
     def _get_weather_data(self, coordinate, start_date, end_date):
         weather_payload = {
             "timezone": "auto",
             "current_weather": "true",
-            "daily": "weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,windspeed_10m_max,winddirection_10m_dominant".split(
-                ","
-            ),
+            "daily": "weathercode,temperature_2m_max,"
+            "temperature_2m_min,sunrise,sunset,"
+            "precipitation_sum,windspeed_10m_max,"
+            "winddirection_10m_dominant".split(","),
+            "latitude": coordinate[0],
+            "longitude": coordinate[1],
+            "start_date": start_date,
+            "end_date": end_date,
         }
-        weather_payload["latitude"] = coordinate[0]
-        weather_payload["longitude"] = coordinate[1]
-        weather_payload["start_date"] = start_date
-        weather_payload["end_date"] = end_date
 
-        r = requests.get(self.weather_API_url, params=weather_payload)
+        response = requests.get(
+            self.weather_API_url, params=weather_payload, timeout=3
+        )  # timeout to avoud throttling
 
-        if r.status_code == 200:
-            return wu.json_convert(r.json())
+        if response.status_code == 200:
+            return wu.json_convert(response.json())
         else:  # error handling
-            if r.status_code == 400:
-                return r.json()["reason"]
+            if response.status_code == 400:
+                return response.json()["reason"]
             else:
-                return f"HTML Error {r.status_code}"
+                return f"HTML Error {response.status_code}"
 
 
 if __name__ == "__main__":
     w = Weather()
 
-    for loc in ["zürich", "zurich", "kljfdsklfsdjkfd"]:
-        print(loc)
-        r = w.get_weather(loc, "2023-01-26", "2023-01-27")
-        print(r["location"])
-        if r["location"] != "Not Found":
-            for x in r["data"]:
-                for y in x:
-                    print(y)
+    # for loc in ["zürich", "zurich", "kljfdsklfsdjkfd"]:
+    #     print(loc)
+    #     r = w.get_weather(loc, "2023-01-26", "2023-01-27")
+    #     time.sleep(5)  # to avoid request throttling
+    #     print(r["location"])
+    #     if r["location"] != "Not Found":
+    #         for x in r["data"]:
+    #             for y in x:
+    #                 print(y)
